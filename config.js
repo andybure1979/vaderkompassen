c558151c06c9030956fa81cf4b3362bdaa0e82dd
andybure1979 <andreas.bure@gmail.com>
2026-07-31 08:09:11 +0200

    Update Supabase configuration values

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ window.VK_CONFIG = Object.freeze({
   apiTimeoutMs: 10000,
   preferCloud: true,
   allowLocalFallback: true,
-  supabaseUrl: "",
-  supabaseAnonKey: ""
+  supabaseUrl: "https://buyeqmczjatsyylvayti.supabase.co",
+  supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ1eWVxbWN6amF0c3l5bHZheXRpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODUwMjQ3NjcsImV4cCI6MjEwMDYwMDc2N30.zHetxZBpflhP5or3eVa_9ZBnQ62OmQiiSi8VCTbWOwg"
 });
